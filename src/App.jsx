67e3f3e99f4dbd1f3ad72fba1fa7292678e9e9b5
Sahuan <sahuanpimentel@gmail.com>
2025-07-22 13:17:03 -0300

    Making components reusable with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header";
+import ComponentImg from "./assets/components.png";
+import UiImg from "./assets/jsx-ui.png";
+import ConfigImg from "./assets/config.png";
+import StateImg from "./assets/state-mgmt.png";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI Building Block."
+              img={ComponentImg}
+            />
+            <CoreConcept
+              title="Config"
+              description="The core UI Building Block."
+              img={ConfigImg}
+            />
+            <CoreConcept
+              title="JSX UI"
+              description="The core UI Building Block."
+              img={UiImg}
+            />
+            <CoreConcept
+              title="State Management"
+              description="The core UI Building Block."
+              img={StateImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
